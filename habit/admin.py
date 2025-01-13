c59ff2e697c9affb07d898b3b4891ecca898e471
parsa <kookyparsa@gmail.com>
2025-01-13 12:46:02 +0330

    admin panal setup

--- habit/admin.py
+++ habit/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+admin.site.register(Habit)
+admin.site.register(TaskTracker)
+admin.site.register(Streak)
+admin.site.register(Achievement)
