2e8bffb3ecac63979b5eccb0af108b816d8ab0b8
parsa <kookyparsa@gmail.com>
2025-01-14 14:20:37 +0330

    fix local setting problem

---
+++ local_settings.py
@@ -0,0 +1,12 @@
+# Configuration for SQLite3
+# If you're using sqlite3:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'sqlite3.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
